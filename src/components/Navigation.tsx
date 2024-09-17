ec7c1d406ba5108d3b7497f294f730730c5cac37
Emmanuel-Bellissimo <118261948+Emmanuel-Bellissimo@users.noreply.github.com>
2024-09-17 11:53:40 +0200

    create navigation bar

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,22 @@
+"use client"
+import Link from "next/link";
+import Image from 'next/image'
+import { TbArrowBigRightFilled } from "react-icons/tb";
+export function Navigation() {
+    return (
+        <div className="sticky top-0 backdrop-blur-xl bg-[rgba(0,0,0,0.8)] border-b border-slate-800 z-50">
+            <div className="px-5 w-full max-w-screen-md m-auto flex justify-between py-5">
+                <Link href="/">
+                    <Image src="/logo-guy.png" alt="Family Guy" width={70} height={50} />
+                </Link>
+                <Link
+                    href="/quiz"
+                    className="flex items-center justify-center gap-1 px-5 font-semibold text-black transition-colors bg-green-500 rounded-md duration-600 hover:bg-green-600"
+                >
+                    <TbArrowBigRightFilled className="text-lg" />
+                    Take a Quiz
+                </Link>
+            </div>
+        </div>
+    )
+}
